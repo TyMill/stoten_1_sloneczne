a2c9efa88a3ba9d13c396ce04d96143188e32fab
TyMill <81069997+TyMill@users.noreply.github.com>
2023-07-03 13:50:27 +0200

    Create fut_importance.py

---
+++ fut_importance.py
@@ -0,0 +1,31 @@
+from sklearn.ensemble import RandomForestRegressor
+
+# Create a new random forest regressor
+rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+
+# Train the random forest regressor
+rf.fit(X_train, y_train)
+
+# Get numerical feature importances
+importances = list(rf.feature_importances_)
+
+# List of tuples with variable and importance
+feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+
+# Sort the feature importances by most important first
+feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+
+# Print out the feature and importances 
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
+
+# list of x locations for plotting
+x_values = list(range(len(importances)))
+
+# Make a bar chart
+plt.bar(x_values, importances, orientation = 'vertical')
+
+# Tick labels for x axis
+plt.xticks(x_values, feature_list, rotation='vertical')
+
+# Axis labels and title
+plt.ylabel('Importance'); plt.xlabel('Variable'); plt.title('Variable Importances');
